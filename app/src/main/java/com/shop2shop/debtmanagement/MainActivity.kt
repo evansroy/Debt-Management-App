9ae3e0b6d9df0908bffe1c1e7e2d8e3c88731912
Evans Roy <evansroysir@gmail.com>
2023-01-03 07:17:06 +0300

    Added drop down menu for profile,settings and logout option

--- app/src/main/java/com/shop2shop/debtmanagement/MainActivity.kt
+++ app/src/main/java/com/shop2shop/debtmanagement/MainActivity.kt
@@ -2,9 +2,14 @@ package com.shop2shop.debtmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,26 @@ class MainActivity : AppCompatActivity() {
         val navController = Navigation.findNavController(this, R.id.hostFragment)
 
         NavigationUI.setupWithNavController(bottomNavigation,navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dropdown_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.profile-> Toast.makeText(this,"Profile Selected",Toast.LENGTH_SHORT).show()
+            R.id.settings->Toast.makeText(this,"Settings Selected",Toast.LENGTH_SHORT).show()
+            R.id.logout->logout()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logout() {
+
 
     }
 }
